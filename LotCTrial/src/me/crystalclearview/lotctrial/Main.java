285fc70c04ba75516c6eb1ecdb05d6f83a90be47
CrystalClearView <57296620+CrystalClearView@users.noreply.github.com>
2020-05-20 22:26:47 +0100

    Update Main.java

--- LotCTrial/src/me/crystalclearview/lotctrial/Main.java
+++ LotCTrial/src/me/crystalclearview/lotctrial/Main.java
@@ -1,10 +1,13 @@
 package me.crystalclearview.lotctrial;
 
-import me.crystalclearview.lotctrial.Commands.CropTramplingCommand;
+import me.crystalclearview.lotctrial.Commands.FarmCommand;
 import me.crystalclearview.lotctrial.Events.CircumventPreventionEvent;
 import me.crystalclearview.lotctrial.Events.CropTramplingEvent;
 import me.crystalclearview.lotctrial.Events.HarvestEvent;
+import me.crystalclearview.lotctrial.Events.InventoryClickEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -13,19 +16,28 @@ public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("LotCTrial Plugin Enabled!");
+
+        if(this.getServer().getPluginManager().getPlugin("Vault") == null){
+            getLogger().warning("Vault not found! Disabling money surprises!");
+        }
         //Loading the config
         getConfig().options().copyDefaults(true);
         saveConfig();
         plugin = this;
         //Registering Commands&Events.
-        getServer().getPluginManager().registerEvents(new HarvestEvent(this), this);
-        getServer().getPluginManager().registerEvents(new CropTramplingEvent(this), this);
-        getServer().getPluginManager().registerEvents(new CircumventPreventionEvent(this), this);
-        this.getCommand("croptrampling").setExecutor(new CropTramplingCommand(this));
+        PluginManager pm = Bukkit.getPluginManager();
+
+        pm.registerEvents(new HarvestEvent(this), this);
+        pm.registerEvents(new CropTramplingEvent(this), this);
+        pm.registerEvents(new CircumventPreventionEvent(this), this);
+        pm.registerEvents(new InventoryClickEvent(this), this);
+
+        this.getCommand("farm").setExecutor(new FarmCommand(this));
     }
     public void onDisable(){
         getLogger().info("LotCTrial Plugin Disabled!");
     }
+
     //Creating a static method to be able to access plugin in any class. (Getter and Setter)
     public static JavaPlugin plugin;
 
