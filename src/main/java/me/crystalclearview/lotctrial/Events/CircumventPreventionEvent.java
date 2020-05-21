36787b54a6cdb30a70523529d22393a126470cf0
CrystalClearView <57296620+CrystalClearView@users.noreply.github.com>
2020-05-21 11:57:49 +0100

    Create CircumventPreventionEvent.java

---
+++ src/main/java/me/crystalclearview/lotctrial/Events/CircumventPreventionEvent.java
@@ -0,0 +1,31 @@
+package me.crystalclearview.lotctrial.Events;
+
+import me.crystalclearview.lotctrial.Main;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPistonExtendEvent;
+import org.bukkit.event.block.BlockPistonRetractEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class CircumventPreventionEvent implements Listener {
+
+    public Main plugin;
+    public CircumventPreventionEvent(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler (ignoreCancelled = true)
+    public void onPistonFarming(BlockPistonExtendEvent event) {
+        for (Block block : event.getBlocks().toArray(new Block[0])) {
+            Material type = block.getType();
+            if (type.equals(Material.WHEAT) || type.equals(Material.POTATOES) || type.equals(Material.CARROTS) || type.equals(Material.BEETROOTS)) {
+                event.setCancelled(true);
+                event.getBlock().setType(Material.AIR);
+                return;
+            }
+        }
+    }
+
+}
